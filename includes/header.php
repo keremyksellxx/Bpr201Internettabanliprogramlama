f67db52dbcb1a8efeed452b0e1418234bae6e299
keremyksellxx <keremyksellx@gmail.com>
2025-10-29 22:57:58 +0300

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>YeniHaber - Dijital Dergi</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">KYHABER</a>
+    <button class="navbar-toggler text-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item"><a class="nav-link active" href="index.php">Anasayfa</a></li>
+        <li class="nav-item"><a class="nav-link" href="kategori.php">Kategoriler</a></li>
+        <li class="nav-item"><a class="nav-link" href="hakkimizda.php">Hakkımızda</a></li>
+        <li class="nav-item"><a class="nav-link" href="iletisim.php">İletişim</a></li>
+        <li class="nav-item"><a class="nav-link" href="login.php">Giriş</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
